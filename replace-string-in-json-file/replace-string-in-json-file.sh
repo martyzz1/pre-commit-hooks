9647f1d5032e414bf12719c072cf1d9573be5273
Martin Moss <martyzz1@gmail.com>
2023-09-11 22:33:30 +0100

    switchup the functions

--- replace-string-in-json-file/replace-string-in-json-file.sh
+++ replace-string-in-json-file/replace-string-in-json-file.sh
@@ -2,8 +2,8 @@
 
 # Path to the package-lock.json
 FILE_PATH="$1"
-FROM_STRING="$2"
+FROM_STRING=$(echo "$2" | sed -e 's/\./\\./g' -e 's/\:/\\:/g')
 TO_STRING="$3"
 
 # Use jq to replace $FROM_STRING with $TO_STRING and overwrite the original file
-jq --arg from "$FROM_STRING" --arg to "$TO_STRING" 'recurse | if type == "string" then gsub($from; $to) else . end' "$FILE_PATH" > "${FILE_PATH}.tmp" && mv "${FILE_PATH}.tmp" "$FILE_PATH"
+jq --arg from "$FROM_STRING" --arg to "$TO_STRING" 'walk(if type == "string" then gsub($from; $to) else . end)' "$FILE_PATH" > "${FILE_PATH}.tmp" && mv "${FILE_PATH}.tmp" "$FILE_PATH"
