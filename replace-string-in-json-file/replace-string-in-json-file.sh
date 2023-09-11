0191dbda7da0f674ac68e064a4ebc8377fee7178
Martin Moss <martyzz1@gmail.com>
2023-09-11 22:49:49 +0100

    make more interesting

--- replace-string-in-json-file/replace-string-in-json-file.sh
+++ replace-string-in-json-file/replace-string-in-json-file.sh
@@ -8,7 +8,14 @@ FILE_PATH="$3"
 # Use jq to replace $FROM_STRING with $TO_STRING and overwrite the original file
 jq --arg from "$FROM_STRING" --arg to "$TO_STRING" 'walk(if type == "string" then gsub($from; $to) else . end)' "$FILE_PATH" > "${FILE_PATH}.tmp"
 
-# Print lines where replacements were made
-diff --unchanged-line-format="" --old-line-format="" "$FILE_PATH" "${FILE_PATH}.tmp"
+# Print the changed lines
+diff --unchanged-line-format="" --old-line-format="FIXED: %l\n" --new-line-format="" "$FILE_PATH" "${FILE_PATH}.tmp" | tee changes.txt
 
+# Print the summary of changed lines
+echo "$(wc -l < changes.txt) lines changed"
+
+# Overwrite the original file with the changes
 mv "${FILE_PATH}.tmp" "$FILE_PATH"
+
+# Clean up
+rm changes.txt
