4c83b6302b7034dab9497efa674cf18bff8efff3
Martin Moss <martyzz1@gmail.com>
2023-09-11 22:38:22 +0100

    add printout

--- replace-string-in-json-file/replace-string-in-json-file.sh
+++ replace-string-in-json-file/replace-string-in-json-file.sh
@@ -5,5 +5,9 @@ FILE_PATH="$1"
 FROM_STRING=$(echo "$2" | sed -e 's/\./\\./g' -e 's/\:/\\:/g')
 TO_STRING="$3"
 
+printf "FILE_PATH=%s" "$FILE_PATH"
+printf "FROM_STRING=%s" "$FROM_STRING"
+printf "TO_STRING=%s" "$TO_STRING"
+
 # Use jq to replace $FROM_STRING with $TO_STRING and overwrite the original file
 jq --arg from "$FROM_STRING" --arg to "$TO_STRING" 'walk(if type == "string" then gsub($from; $to) else . end)' "$FILE_PATH" > "${FILE_PATH}.tmp" && mv "${FILE_PATH}.tmp" "$FILE_PATH"
