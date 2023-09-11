79459e829b49ed68c26956f98dc00e0aaa39e7d8
Martin Moss <martyzz1@gmail.com>
2023-09-11 22:45:01 +0100

    more fixing

--- replace-string-in-json-file/replace-string-in-json-file.sh
+++ replace-string-in-json-file/replace-string-in-json-file.sh
@@ -5,9 +5,10 @@ FROM_STRING=$(echo "$1" | sed -e 's/\./\\./g' -e 's/\:/\\:/g')
 TO_STRING="$2"
 FILE_PATH="$3"
 
-printf "FILE_PATH=%s" "$FILE_PATH"
-printf "FROM_STRING=%s" "$FROM_STRING"
-printf "TO_STRING=%s" "$TO_STRING"
-
 # Use jq to replace $FROM_STRING with $TO_STRING and overwrite the original file
-jq --arg from "$FROM_STRING" --arg to "$TO_STRING" 'walk(if type == "string" then gsub($from; $to) else . end)' "$FILE_PATH" > "${FILE_PATH}.tmp" && mv "${FILE_PATH}.tmp" "$FILE_PATH"
+jq --arg from "$FROM_STRING" --arg to "$TO_STRING" 'walk(if type == "string" then gsub($from; $to) else . end)' "$FILE_PATH" > "${FILE_PATH}.tmp"
+
+# Print lines where replacements were made
+diff --unchanged-line-format="" --old-line-format="" "$FILE_PATH" "${FILE_PATH}.tmp"
+
+mv "${FILE_PATH}.tmp" "$FILE_PATH"
